c948a3726861660291225fe0b729ed33b69ae019
Armando <armando@example.com>
2017-01-03 18:08:45 +0100

    Repository Creation

---
+++ config.h
@@ -0,0 +1,22 @@
+/* config.h.  Generated automatically by configure.  */
+/* config.h.in.  Generated automatically from configure.in by autoheader 2.13.  */
+
+/* Define if you have the ANSI C header files.  */
+#define STDC_HEADERS 1
+
+/* #undef ENABLE_NLS */
+/* #undef HAVE_CATGETS */
+/* #undef HAVE_GETTEXT */
+/* #undef HAVE_LC_MESSAGES */
+/* #undef HAVE_STPCPY */
+/* #undef HAVE_LIBSM */
+/* #undef PACKAGE_LOCALE_DIR */
+#define PACKAGE_DATA_DIR "/usr/local/share/project6"
+#define PACKAGE_SOURCE_DIR "/root/Projects/project9"
+
+/* Name of package */
+#define PACKAGE "project6"
+
+/* Version number of package */
+#define VERSION "0.1"
+
